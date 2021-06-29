41fbb18cc3176a73ec4d020541f127486c38dc56
Luke Shin <shingun@ideasam.net>
2021-06-29 15:50:54 +0900

    Go 기본 제공 String() 변경예제

--- main.go
+++ main.go
@@ -9,10 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("Luke")
 	account.Deposit(10)
-	err := account.Withdraw(20)
-	if err != nil {
-		// log.Fatalln(err)
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
 }
