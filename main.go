6987a29993e95890dca5892130da02b57cdf8446
Luke Shin <shingun@ideasam.net>
2021-06-29 22:41:53 +0900

    Dictionary Search Test

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/usrname/learngo/accounts"
+	"github.com/usrname/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("Luke")
-	account.Deposit(10)
-	fmt.Println(account)
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 }
